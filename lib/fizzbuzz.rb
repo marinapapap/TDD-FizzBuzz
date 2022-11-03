7f018b5cebb8e41d5053c1527767304d9483dd34
Marina Papapetrou <contact@marinapapap.com>
2022-11-03 15:02:58 +0000

    Passing tests up to multiples of 3 and 5

--- lib/fizzbuzz.rb
+++ lib/fizzbuzz.rb
@@ -1,9 +1,11 @@
 def fizzbuzz(number)
-  if number == 3
+  if number % 3 == 0 && number % 5 == 0
+    "fizzbuzz"
+  elsif number % 3 == 0
     "fizz"
-  elsif number == 5
+  elsif number % 5 == 0
     "buzz"
-  elsif number == 1
-     1
+  else
+     number
   end
 end 
--- spec/fizzbuzz_spec.rb
+++ spec/fizzbuzz_spec.rb
@@ -12,4 +12,25 @@ describe 'fizzbuzz' do
   it 'returns 1 when passed 1' do
     expect(fizzbuzz(1)).to eq 1
   end
+  
+  it 'returns "fizz" when passed multiple of 3' do
+    expect(fizzbuzz(6)).to eq 'fizz'
+    expect(fizzbuzz(9)).to eq 'fizz'
+  end
+  
+  it 'returns "buzz" when passed multiple of 5' do
+    expect(fizzbuzz(10)).to eq 'buzz'
+    expect(fizzbuzz(20)).to eq 'buzz'
+  end
+  
+  it 'returns the number when number passed is not multiple of 3 or 5' do
+    expect(fizzbuzz(7)).to eq 7
+    expect(fizzbuzz(14)).to eq 14
+  end
+  
+  it 'returns "fizzbuzz" when passed multiple of 3 and 5' do
+    expect(fizzbuzz(15)).to eq 'fizzbuzz'
+    expect(fizzbuzz(30)).to eq 'fizzbuzz'
+  end
+  
 end
